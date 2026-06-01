ff592a2894d30a598ff24859295843c48a09ea0e
Matos52 <Matej.Regec@gmail.com>
2026-06-01 19:17:16 +0200

    small refactoring

--- src/components/ui/AccommodationCard.jsx
+++ src/components/ui/AccommodationCard.jsx
@@ -6,13 +6,13 @@ const AccommodationCard = ({ image, title, rooms, beds, extraBeds, slug }) => {
       <img
         src={image}
         alt={title}
-        className="w-full h-auto object-cover mb-14 rounded-2xl"
+        className="w-full h-auto md:h-[470px] object-cover mb-14 rounded-2xl"
       />
 
       <div className="absolute left-0 right-0 bottom-0 flex justify-center">
-        <div className="bg-white w-4/5 px-4 py-3 shadow-md hover:shadow-lg">
+        <div className="bg-white w-4/5 rounded-lg px-4 py-3 shadow-md hover:shadow-lg">
           <Link to={`/accommodation/${slug}`} className="group">
-            <h2 className="relative inline-block text-lg md:text-xl font-semibold text-gray-800 transition-colors duration-300 group-hover:text-gray-700">
+            <h2 className="relative inline-block text-base md:text-xl font-semibold text-gray-800 transition-colors duration-300 group-hover:text-gray-700">
               {title}
               <span className="absolute left-0 -bottom-0 h-[2px] w-0 bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
             </h2>
