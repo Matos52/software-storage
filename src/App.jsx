15d4d29ba2225b296f2dcd9b49240ae542110122
Matos52 <Matej.Regec@gmail.com>
2026-06-01 19:25:22 +0200

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
-import HomePage from "./pages/Homepage";
+import HomePage from "./pages/HomePage";
 import AccommodationDetailPage from "./pages/AccommodationDetailPage";
 
 function App() {
